b7b3326774f7307acde7aaae653664ca5342e4e5
Chris Wike <chriswike@gmail.com>
2022-07-25 18:02:26 -0500

    modified during leacture

--- exercises/parts1-5.ts
+++ exercises/parts1-5.ts
@@ -18,15 +18,10 @@ let daysToMars: number = hoursToMars / 24;
 
 // Code an output statement here (use a template literal):
 
-
 // Part 3: Create a Function ("getDaysToLocation")
-
-
 
 // Move your output statement from part 2 here. Update the template literal to call
 // the function and print the outputs for a Mars trip and a moon trip.
-
-
 
 // Part 4: Create a Spacecraft Class
 class Spacecraft {
@@ -50,20 +45,14 @@ class Spacecraft {
     }
 }
 
-
-
 // Create an instance of the class here:
 
 let spaceShuttle = new Spacecraft('Determination', 17500);
-
-
 
 // Move your output statements from part 3 here. Update the template literals use the
 // instance of the class.
 // console.log(`${spaceShuttle.name} would take ${spaceShuttle.getDaysToLocation(kilometersToMars)} days to get to Mars.`);
 // console.log(`${spaceShuttle.name} would take $${spaceShuttle.getDaysToLocation(kilometersToTheMoon)} days to get to the Moon`)
-
-
 
 // Part 5: Export and Import the SpaceLocation Class
 // Add the required import statement BEFORE the part 1 concent.
